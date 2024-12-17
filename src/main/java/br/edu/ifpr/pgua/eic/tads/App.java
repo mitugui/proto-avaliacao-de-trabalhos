0c67a615f91a2a3e8bb60667eb43cfff2ee90cd7
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-12-17 16:30:19 -0300

    feat: inicializar projeto Javalin com estrutura básica e dependências - Configurado o projeto Maven com dependências do Javalin, Freemarker e SLF4J. - Adicionada estrutura básica com servidor, controlador e utilitários. - Incluídos arquivos públicos index.html e estilo.css. - Criado teste unitário básico e .gitignore.

---
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/App.java
@@ -0,0 +1,14 @@
+package br.edu.ifpr.pgua.eic.tads;
+
+import br.edu.ifpr.pgua.eic.tads.controllers.IndexController;
+import br.edu.ifpr.pgua.eic.tads.utils.JavalinUtils;
+
+public class App {
+    public static void main( String[] args ){
+        var app = JavalinUtils.makeApp(7070);
+        
+        IndexController indexController = new IndexController();
+        
+        app.get("/", indexController.get);
+    }
+}
