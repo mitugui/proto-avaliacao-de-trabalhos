d3dec338c85cb6cc59784c12b63252a14e41adc6
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-12-19 18:31:19 -0300

    feat: adicionar configuração de rotas para cadastro de usuário no App - O App agora inicializa e configura as rotas para o `UsuarioController` com as funcionalidades de cadastro de usuário. - A rota `GET /cadastro` exibe o formulário de cadastro de usuário. - A rota `POST /cadastro/usuario` processa o cadastro de um novo usuário. - O `UsuarioController` é inicializado com a `UsuarioService`, que por sua vez depende da `UsuarioDAO`, configurada para usar a `FabricaDeConexoes` para gerenciar a conexão com o banco de dados.

--- src/main/java/br/edu/ifpr/pgua/eic/tads/App.java
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/App.java
@@ -1,6 +1,9 @@
 package br.edu.ifpr.pgua.eic.tads;
 
 import br.edu.ifpr.pgua.eic.tads.controllers.IndexController;
+import br.edu.ifpr.pgua.eic.tads.controllers.UsuarioController;
+import br.edu.ifpr.pgua.eic.tads.usuario.UsuarioDAO;
+import br.edu.ifpr.pgua.eic.tads.usuario.UsuarioService;
 import br.edu.ifpr.pgua.eic.tads.utils.JavalinUtils;
 
 public class App {
@@ -8,7 +11,11 @@ public class App {
         var app = JavalinUtils.makeApp(7070);
         
         IndexController indexController = new IndexController();
+        UsuarioController usuarioController = new UsuarioController(new UsuarioService(new UsuarioDAO(FabricaDeConexoes.getConnection())));
         
         app.get("/", indexController.get);
+
+        app.get("/cadastro", usuarioController.get);
+        app.post("/cadastro/usuario", usuarioController.post);
     }
 }
