5a423bbeb488acb5491aec1e8fd2ec9bb62705e9
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-12-19 18:21:47 -0300

    feat: criar a DAO UsuarioDAO para gerenciar a comunicação com o banco de dados e salvar usuários - Implementada a classe UsuarioDAO com a responsabilidade de comunicação com o banco de dados. - O método salvar insere os dados do usuário na tabela usuario com as colunas nome, email e senha. - A classe utiliza a fábrica de conexões para obter a conexão com o banco de dados.

---
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/usuario/UsuarioDAO.java
@@ -0,0 +1,42 @@
+package br.edu.ifpr.pgua.eic.tads.usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UsuarioDAO {
+    private Connection conn;
+
+    public UsuarioDAO(Connection connection) {
+        this.conn = connection;
+    }
+
+    public boolean salvar(DadosUsuarioCadastro usuario) {
+        PreparedStatement pstm = null;
+
+        try {
+            String sql = "INSERT INTO usuario(nome, email, senha) VALUES (?, ?, ?)";
+
+            pstm = conn.prepareStatement(sql);
+
+            pstm.setString(1, usuario.nome());
+            pstm.setString(2, usuario.email());
+            pstm.setString(3, usuario.senha());
+
+            return pstm.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao cadastrar usuário no banco de dados.", e);
+        } finally {
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar recursos: " + e.getMessage());                
+            }
+        }
+    }
+}
