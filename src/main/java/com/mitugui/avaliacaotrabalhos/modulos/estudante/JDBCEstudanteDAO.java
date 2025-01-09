c5efebc56f3ba318570e70965026431d06b80871
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2025-01-08 22:28:38 -0300

    feat: implementar verificação no cadastro de estudante para impedir que usuário com vínculo com professor seja cadastrado como estudante

--- src/main/java/com/mitugui/avaliacaotrabalhos/modulos/estudante/JDBCEstudanteDAO.java
+++ src/main/java/com/mitugui/avaliacaotrabalhos/modulos/estudante/JDBCEstudanteDAO.java
@@ -2,6 +2,7 @@ package com.mitugui.avaliacaotrabalhos.modulos.estudante;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mitugui.avaliacaotrabalhos.exceptions.ConexaoBancoException;
@@ -16,15 +17,27 @@ public class JDBCEstudanteDAO implements EstudanteDAO {
 
     @Override
     public boolean salvar(DadosCadastroEstudante estudante, Integer usuario_id) throws SQLException {
-        String sql = "INSERT INTO estudante(usuario_id, curso_id, ano_ingresso, matricula) VALUES (?, ?, ?, ?);";
+        String sqlVerificacao = "SELECT 1 FROM professor WHERE usuario_id = ?";
 
-        try(PreparedStatement pstm = conn.prepareStatement(sql)){
-            pstm.setInt(1, usuario_id);
-            pstm.setInt(2, 1);
-            pstm.setInt(3, estudante.anoIngresso());
-            pstm.setString(4, estudante.matricula());
+        try(PreparedStatement pstmVerificacao = conn.prepareStatement(sqlVerificacao)) {
+            pstmVerificacao.setInt(1, usuario_id);
 
-            return pstm.executeUpdate() == 1;
+            try (ResultSet rs = pstmVerificacao.executeQuery()) {
+                if (!rs.next()) {
+                    String sqlCadastro = "INSERT INTO estudante(usuario_id, curso_id, ano_ingresso, matricula) VALUES (?, ?, ?, ?);";
+
+                    try (PreparedStatement pstmCadastro = conn.prepareStatement(sqlCadastro)) {
+                        pstmCadastro.setInt(1, usuario_id);
+                        pstmCadastro.setInt(2, 1);
+                        pstmCadastro.setInt(3, estudante.anoIngresso());
+                        pstmCadastro.setString(4, estudante.matricula());
+                        
+                        return pstmCadastro.executeUpdate() == 1;  
+                    }
+                } else {
+                    return false;
+                }
+            }
         } catch (SQLException e) {
             throw new ConexaoBancoException("Erro na conexão com o banco de dados ao cadastrar estudante.", e);
         }
